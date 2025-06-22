4c508c986feb695dcfa175993626fc4ac0c066be
luca2010stau <stauluca2000@gmail.com>
2025-06-23 01:26:47 +0200

    Delete test-mail.php

--- test-mail.php
+++
@@ -1,4 +0,0 @@
-<?php
-$test = mail("l.staurenghi@konsulto.it", "Test invio da PHP", "Se stai leggendo questa email, il server funziona correttamente.");
-echo $test ? "✅ Email inviata! Il server supporta mail()." : "❌ Invio fallito. Probabile blocco della funzione mail() su questo hosting.";
-?>
